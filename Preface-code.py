9270d5fb6ca14612286cd2420730bfce06e79cbf
adii1823 <64855277+adii1823@users.noreply.github.com>
2021-08-18 15:22:13 +0530

    Create Preface-code.py

---
+++ Preface-code.py
@@ -0,0 +1,14 @@
+2**50
+
+print({1, 2, 3, 2, 4,
+       1, 5, 3, 6, 7,
+       1, 3, 8, 2})
+
+def display_string(the_string, put_in_uppercase=False):
+    if put_in_uppercase:
+        print(the_string.upper())
+    else:
+        print(the_string)
+
+print("YOU SHOULD NOT BE ABLE TO SEE THIS")
+
